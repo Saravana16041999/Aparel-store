8362c0c1ac0d3be6cff27420f6689c10c4a90d95
Saravana16041999 <109520321+Saravana16041999@users.noreply.github.com>
2023-01-06 06:49:41 +0530

    Update Valhala.js

--- src/components/Valhala.js
+++ src/components/Valhala.js
@@ -4,7 +4,7 @@ const Valhala = ({image,title,discription,prize}) => {
     // const {image,title,discription,prize} = props
     return (
       <section className='product'>
-        <img className='jacket_img' src={image} />
+        <img className='jacket_img' src={image} alt = ""/>
         <h3>{title}</h3>
         <h4>{discription}</h4>
         <h3>{prize}</h3>
